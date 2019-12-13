5c163182cb7573f120face9ce66aa9f94f2e80a2
Gaven Royer <gavroyer@gmail.com>
2019-12-13 10:06:25 -0700

    Update Window

--- ad1459/widgets/window.py
+++ ad1459/widgets/window.py
@@ -22,4 +22,32 @@ class AdWindow(Gtk.Window):
         header = Headerbar()
         self.set_titlebar(header)
 
+        maingrid = Gtk.Grid()
+        self.add(maingrid)
+
+        content = Gtk.HPaned()
+        content.set_position(300)
+        content.set_hexpand(True)
+        content.set_vexpand(True)
+        maingrid.attach(content, 0, 0, 1, 1)
+
+        servers_grid = Gtk.Grid()
+        content.add1(servers_grid)
+        servers_window = Gtk.ScrolledWindow()
+        servers_grid.attach(servers_window, 0, 0, 1, 1)
+
+        self.servers_listbox = Gtk.ListBox()
+        servers_grid.attach(self.servers_listbox, 0, 0, 1, 1)
+
+        test_message = Gtk.Grid()
+        test_message.set_hexpand(True)
+        test_message.props.margin = 12 
+        test_message.set_column_spacing(12)
+        content.add2(test_message)
+
+        test_msg_time = Gtk.Label()
+        test_msg_time.set_text('25:64')
+        test_message.attach(test_msg_time, 0, 0, 1, 1)
+
+
         
