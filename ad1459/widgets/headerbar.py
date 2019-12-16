bafe12d7d681471175582332128f58f9b7649280
Gaven Royer <gavroyer@gmail.com>
2019-12-16 16:40:33 -0700

    Add docstrings

--- ad1459/widgets/headerbar.py
+++ ad1459/widgets/headerbar.py
@@ -14,6 +14,7 @@ gi.require_version('Gtk', '3.0')
 from gi.repository import Gtk
 
 class Headerbar(Gtk.HeaderBar):
+    """ The headerbar we use."""
 
     def __init__(self):
         super().__init__()
