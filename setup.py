e2babbc894e3c1cf1e541be5faea2e93bd43f27f
Gaven Royer <gavroyer@gmail.com>
2019-12-26 14:43:57 -0700

    feat: Add release command to setup.py

--- setup.py
+++ setup.py
@@ -9,11 +9,45 @@
   This is the Python package.
 """
 
-from setuptools import setup, find_packages
+from setuptools import setup, find_packages, Command
+import subprocess
 
 version = {}
 with open('ad1459/__version__.py') as fp:
   exec(fp.read(), version)
+
+class Release(Command):
+    """ Generate a release and push it to git."""
+    description = "Generate a release and push it to git."
+
+    user_options = [
+        ('dry-run', None, 'Skip the actual release and do a dry run instead.'),
+        ('prerelease', None, 'Release this version as a pre-release.'),
+        ('force-version', None, 'Force the version to update to the given value.')
+    ]
+
+    def initialize_options(self):
+        self.dry_run = False
+        self.prerelease = False
+        self.force_version = None
+    
+    def finalize_options(self):
+        pass
+
+    def run(self):
+        command = ['npx', 'standard-version@next']
+        if self.dry_run:
+            command.append('--dry-run')
+        if self.prerelease:
+            command.append('--prerelease')
+        if self.force_version:
+            # See https://github.com/conventional-changelog/standard-version#release-as-a-target-type-imperatively-npm-version-like
+            pass
+        subprocess.run(command)
+        if not self.dry_run:
+            subprocess.run(
+                ['git', 'push', '--follow-tags']
+            )
 
 setup(
     name='ad1459',
@@ -30,6 +64,9 @@ setup(
     data_files=[
       ('/usr/share/applications', ['data/in.donotspellitgav.ad1459.desktop']),
     ],
+
+    # Commands
+    cmdclass={'release': Release},
 
     # Project Metadata
     author='Gaven Royer',
